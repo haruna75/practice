c39e5790c00169b0898b1c29516a2a91cb0137c7
haruna75 <91399000+haruna75@users.noreply.github.com>
2024-07-04 23:35:34 +0900

    Add files via upload

--- script.py
+++ script.py
@@ -1,8 +1,11 @@
-import service
+import utils
 
 print ('複利計算を始めましょう！ただし、複利が発生するのは1年ごととします！')
 principal_user = input ('元本を教えてください：')
 rate_user = input ('複利が何％か教えてください：')
 period_user = input ('何年分の計算をしますか？：')
 
-result = service.compound_interest(principal_user, rate_user, period_user)
+print ( '元本が' + principal_user + 'で、複利が' + rate_user + '%の時の、' + period_user + '年後の額を計算しますね！')
+
+result = utils.compound_interest(principal_user, rate_user, period_user)
+print ('答えは' + str(result) + 'ですね！')
