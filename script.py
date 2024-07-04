551a5e2bceea8f8f5907aef4c6862a7c378f0ad4
haru <spring.erin75@gmail.com>
2024-07-04 22:47:13 +0900

    docs: 標準出力の文言部分を削除しました

--- script.py
+++ script.py
@@ -5,7 +5,4 @@ principal_user = input ('元本を教えてください：')
 rate_user = input ('複利が何％か教えてください：')
 period_user = input ('何年分の計算をしますか？：')
 
-print ( '元本が' + principal_user + 'で、複利が' + rate_user + '%の時の、' + period_user + '年後の額を計算しますね！')
-
 result = service.compound_interest(principal_user, rate_user, period_user)
-print ('答えは' + str(result) + 'ですね！')
