6d7d1fdb6d238fe57a22a21989845bc085522f1b
haru <spring.erin75@gmail.com>
2024-07-04 22:39:12 +0900

    doc: utilsをserviceに変更しました！(ファイル名は再アップに伴って最初に変更済み)

--- script.py
+++ script.py
@@ -1,4 +1,4 @@
-import utils
+import service
 
 print ('複利計算を始めましょう！ただし、複利が発生するのは1年ごととします！')
 principal_user = input ('元本を教えてください：')
@@ -7,5 +7,5 @@ period_user = input ('何年分の計算をしますか？：')
 
 print ( '元本が' + principal_user + 'で、複利が' + rate_user + '%の時の、' + period_user + '年後の額を計算しますね！')
 
-result = utils.compound_interest(principal_user, rate_user, period_user)
+result = service.compound_interest(principal_user, rate_user, period_user)
 print ('答えは' + str(result) + 'ですね！')
