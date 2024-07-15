a86dc8ac7289d407c0ab0b5208958e2afbdcdba0
haru <spring.erin75@gmail.com>
2024-07-16 00:05:03 +0900

    fix: Flaskの仕組み上、参照するHTMLファイルはtemplatesというディレクトリの下にあることが必要だったので、変更しました！

--- script.py
+++
@@ -1,8 +0,0 @@
-import service
-
-print ('複利計算を始めましょう！ただし、複利が発生するのは1年ごととします！')
-principal_user = input ('元本を教えてください：')
-rate_user = input ('複利が何％か教えてください：')
-period_user = input ('何年分の計算をしますか？：')
-
-result = service.compound_interest(principal_user, rate_user, period_user)
